c7539a4d9f63fa92c03332eb5e3e9742c4e0b6da
Daniel <d.olugbade@outlook.com>
2020-01-29 17:11:54 +0000

    Established connection between front end and back end

--- client/src/components/book-suggestion-view.component.js
+++ client/src/components/book-suggestion-view.component.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function BookSuggestion() {
+
+    fetch(`http://localhost:2700/books`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+
     const greeting = 'Book Suggestion Component';
     return <h1>{greeting}</h1>;
 }
